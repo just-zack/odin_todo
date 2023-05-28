d7b03345314612582328f2d40b05a5b6b60f7765
Zach_Huang <128434708+zach-huang@users.noreply.github.com>
2023-05-27 16:05:07 -1000

    Update layout on createTaskCard function

--- src/displayHandler.js
+++ src/displayHandler.js
@@ -1,8 +1,8 @@
 function createTaskCard(currentTask, currentProject, homeProject) {
   const taskCard = document.createElement("div");
+  const titleDescriptionContainer = document.createElement("div");
   const taskContent = document.createElement("div");
-  const taskRow1 = document.createElement("div");
-  const taskRow2 = document.createElement("div");
+  const leftContainer = document.createElement("div");
   const taskName = document.createElement("h3");
   const taskDate = document.createElement("h3");
   const taskUrgency = document.createElement("h3");
@@ -11,46 +11,51 @@ function createTaskCard(currentTask, currentProject, homeProject) {
 
   taskCard.setAttribute("id", "t_" + currentTask.name);
   taskCard.classList.add("task_card");
+  titleDescriptionContainer.classList.add("task_title_description");
+
   taskContent.classList.add("task_content");
   doneBtn.classList.add("done_btn");
-  taskCard.appendChild(doneBtn);
-  taskCard.appendChild(taskName);
-  taskCard.appendChild(taskRow1);
+  leftContainer.classList.add("left_task_container");
+  taskCard.appendChild(leftContainer);
+  leftContainer.appendChild(doneBtn);
+  leftContainer.appendChild(titleDescriptionContainer);
+  taskCard.appendChild(taskContent);
+  titleDescriptionContainer.appendChild(taskName);
+  taskName.innerText = "Task: " + currentTask.name;
+  titleDescriptionContainer.appendChild(taskDescription);
   doneBtn.addEventListener("click", () => {
     taskName.classList.toggle("done");
+    taskDescription.classList.toggle("done");
   });
 
-  taskCard.appendChild(taskContent);
-  taskRow1.classList.add("task_row");
-  taskRow2.classList.add("task_row");
-  taskName.innerText = "Task: " + currentTask.name;
-  taskDate.innerText = "Due Date: " + currentTask.date;
-  taskDate.style.color = "#2ec4b6git p";
-  taskRow1.appendChild(taskDate);
-  taskContent.appendChild(taskRow1);
-  taskUrgency.innerText = currentTask.urgency;
-  taskRow1.appendChild(taskUrgency);
-  taskContent.appendChild(taskRow2);
+  if (currentTask.date !== "") {
+    taskDate.innerText = "Due Date: " + currentTask.date;
+    taskContent.appendChild(taskDate);
+  }
+  taskUrgency.innerText = currentTask.urgency.toUpperCase();
+  if (currentTask.urgency === "urgent") {
+    taskUrgency.style.color = "red";
+  } else if (currentTask.urgency === "moderate") {
+    taskUrgency.style.color = "orange";
+  } else {
+    taskUrgency.style.color = "#2ec4b6";
+  }
+  taskContent.appendChild(taskUrgency);
   taskDescription.innerText = "Description: " + currentTask.description;
-  taskRow2.appendChild(taskDescription);
 
   const deleteButton = document.createElement("button");
-
   deleteButton.innerText = "Delete";
   deleteButton.setAttribute("id", currentTask.name + "_delete");
   deleteButton.classList.add("delete_task");
-  taskCard.appendChild(deleteButton);
+  taskContent.appendChild(deleteButton);
   deleteButton.addEventListener("click", () => {
     taskCard.remove();
-    console.log(homeProject);
     removeTaskFromArray(currentTask, currentProject, homeProject);
   });
   return taskCard;
 }
 
 function removeTaskFromArray(currentTask, currentProject, homeProject) {
-  console.log(homeProject);
-  console.log(currentProject);
   if (currentProject.name !== homeProject.name) {
     for (let i = 0; i < currentProject.taskArray.length; i++) {
       if (currentProject.taskArray[i] === currentTask) {
